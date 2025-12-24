5da6bc65f71beeeca3dcd78337dc6d92927daa18
willGyu <jgt8731@itwillbs.co.kr>
2025-12-24 11:37:25 +0900

    컨트롤러에 test1기능을 추가

--- SBG/src/main/java/com/itwillbs/SBG/BootController.java
+++ SBG/src/main/java/com/itwillbs/SBG/BootController.java
@@ -1,8 +1,18 @@
 package com.itwillbs.SBG;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BootController {
+	
+	@RequestMapping(value = "/test1",method = RequestMethod.GET)
+	public String test1() throws Exception{
+		
+		//기능1번 구현
+		
+		return "test";
+	}
 
 }
