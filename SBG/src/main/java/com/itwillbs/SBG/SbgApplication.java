862f22187221881fd88e8669e19733474d90a21f
willGyu <jgt8731@itwillbs.co.kr>
2025-12-24 11:11:30 +0900

    프로젝트 생성 한줄로 쭉~쓰는게 좋음

---
+++ SBG/src/main/java/com/itwillbs/SBG/SbgApplication.java
@@ -0,0 +1,13 @@
+package com.itwillbs.SBG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbgApplication.class, args);
+	}
+
+}
---
+++ SBG/src/test/java/com/itwillbs/SBG/SbgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itwillbs.SBG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
